cb27dd02bacfc6083909bd16085f2a3e9d6bbfe3
Zhyldyz <zsultanova03@gmail.com>
2022-02-04 07:23:16 +0100

    app css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>0</h1>
+        <div>
+        <label htmlFor=""lower value></label>
+        <input type="number" placeholder="enter lower value"/>
+        </div>
+        <div>
+        <label htmlFor=""upper value></label>
+        <input type="number" placeholder="enter upper value"/>
+        </div>
+        <button className="generate">generate</button>
+        <button className="clear">clear</button>
+        
+
+
+      </div>
+    )
+  }
 }
-
-export default App;
