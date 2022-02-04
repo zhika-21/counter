3440f983a953b62a1e0196d6ad306d8f8be4fee4
Zhyldyz <zsultanova03@gmail.com>
2022-02-04 08:49:47 +0100

    add js added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React, {Component} from "react";
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
+    this.state = {
+      minVal: "",
+      maxVal: "",
+      number: 0
+    }
   }
 
-  render(){
+  getMinVal = (e) => {
+    let newMinVal = Number(e.target.value)
+    this.setState({minVal: newMinVal})
+  }
+
+  getMaxVal = (e) => {
+    let newMaxVal = Number(e.target.value)
+    this.setState({maxVal: newMaxVal})
+  }
+
+  generateNum = () => {
+    const {minVal, maxVal} = this.state
+    if (minVal < maxVal) {
+      let randomNum = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal)
+      this.setState({ number: randomNum})
+    } else {
+      alert("invalid input")
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      minVal: "",
+      maxVal: "",
+      number: 0
+    })
+  }
+
+  render() {
+    const { number, minVal, maxVal } = this.state;
     return (
       <div className="container">
-        <h1>0</h1>
-        <div>
-        <label htmlFor=""lower value></label>
-        <input type="number" placeholder="enter lower value"/>
-        </div>
-        <div>
-        <label htmlFor=""upper value></label>
-        <input type="number" placeholder="enter upper value"/>
-        </div>
-        <button className="generate">generate</button>
-        <button className="clear">clear</button>
-        
+        <h1>{number}</h1>
+        <Input labelName="lower value" func={this.getMinVal} value={minVal}/>
 
-
+        <Input labelName="upper value" func={this.getMaxVal} value={maxVal}/>
+        <Button name="generate" func={this.generateNum}/> 
+        <Button name="clear" func={this.clear}/> 
       </div>
-    )
+    );
   }
 }
